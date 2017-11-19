234d8ea92192129607ea572d1895721887e66429
李满满 <xmdevops@vip.qq.com>
2017-11-19 19:04:20 +0800

    :underage: gracefull closing with signal

--- upgradeclient/service/check_service.py
+++ upgradeclient/service/check_service.py
@@ -35,6 +35,9 @@ class CheckService(object):
         self.filter_factory = filter_factory
 
     def sub_process_signal_callback(self, signal_num, unused_frame):
+        print '='*100
+        print signal_num, signal.SIGINT, signal.SIGTERM
+        print '='*100
         if signal_num in (signal.SIGINT, signal.SIGTERM):
             return
         for name in self.sub_process:
