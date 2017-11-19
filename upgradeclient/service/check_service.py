11ddc471fdd66453262723d5334611c52764f2ed
李满满 <xmdevops@vip.qq.com>
2017-11-19 21:55:33 +0800

    :underage: pprint dict(obj)

--- upgradeclient/service/check_service.py
+++ upgradeclient/service/check_service.py
@@ -74,8 +74,7 @@ class CheckService(object):
 
     def send_cache_task(self, obj):
         print '*' * 50
-        pprint.pprint(obj.__dict__)
-        print obj.filename, obj.download_url
+        pprint.pprint(dict(obj))
         print '*' * 50
 
     def handle(self, name, ins):
