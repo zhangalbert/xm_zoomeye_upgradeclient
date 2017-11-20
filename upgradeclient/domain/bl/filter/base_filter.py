19fa0e168fc1d98b5de813d4e2fd1419e875f0ac
李满满 <xmdevops@vip.qq.com>
2017-11-20 13:51:38 +0800

    :underage: limit OEM firmware with base_filter

--- upgradeclient/domain/bl/filter/base_filter.py
+++ upgradeclient/domain/bl/filter/base_filter.py
@@ -7,6 +7,7 @@ from upgradeclient.domain.common.filter import Q, R
 class BaseFilter(object):
     def release_note_validate(self, obj):
         q = Q(obj__filename__regexp='^ReleaseNote') & \
-            Q(obj__download_url__regexp=r'ReleaseNote$')
+            Q(obj__download_url__regexp=r'ReleaseNote$') & \
+            Q(obj__filetype__not_exact='OEM')
 
         return R(obj, q_ins=q)()
