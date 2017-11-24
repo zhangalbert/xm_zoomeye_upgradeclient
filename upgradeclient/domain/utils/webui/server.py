02e1640a163aacee086f8966d2f79ee4eb157a25
李满满 <xmdevops@vip.qq.com>
2017-11-24 23:20:54 +0800

    :underage: perfect webui service

--- upgradeclient/domain/utils/webui/server.py
+++ upgradeclient/domain/utils/webui/server.py
@@ -14,7 +14,7 @@ app = web.application(urls, globals())
 render = web.template.render('{0}/'.format(template_dir), cache=False)
 # 自定义404/500异常
 app.notfound = lambda: web.notfound(render.error(content='not found'))
-app.internalerror = lambda: web.internalerror(content='internal error')
+app.internalerror = lambda: web.internalerror(render.error(content='internal error'))
 
 
 def run(*middleware, **envs):
