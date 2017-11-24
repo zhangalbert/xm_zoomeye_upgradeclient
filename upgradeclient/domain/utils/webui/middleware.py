d9fac717af54fdfbe6731de1db89f96ef8a833e4
李满满 <xmdevops@vip.qq.com>
2017-11-24 21:53:09 +0800

    :underage: perfect webui service

--- upgradeclient/domain/utils/webui/middleware.py
+++ upgradeclient/domain/utils/webui/middleware.py
@@ -20,16 +20,6 @@ class LogMiddleware(WsgiLog):
     def __init__(self, application):
         WsgiLog.__init__(self, application, **env)
 
-    def __call__(self, environ, start_response):
-        if self.log:
-            environ[LOGGERID] = self.logger
-        environ[CATCHID] = self.catch
-        if THROWERR in environ:
-            return self.application(environ, start_response)
-        try:
-            return self.application(environ, start_response)
-        except:
-            return render.error(content=self.catch(environ, start_response))
-    
+
 
 
--- upgradeclient/domain/utils/webui/server.py
+++ upgradeclient/domain/utils/webui/server.py
@@ -5,11 +5,17 @@ import web
 
 
 from upgradeclient.domain.utils.webui.views import *
+from upgradeclient.domain.utils.webui.config import template_dir
 from upgradeclient.domain.utils.webui.middleware import LogMiddleware
 from upgradeclient.domain.utils.webui.urls import url_patterns as urls
 
 
 app = web.application(urls, globals())
+render = web.template.render('{0}/'.format(template_dir), cache=False)
+
+# 自定义404/500页面
+app.notfound = lambda e: web.notfound(render.error(content=e))
+app.internalerror = lambda e: web.internalerror(render.error(content=e))
 
 
 def run(*middleware, **envs):
