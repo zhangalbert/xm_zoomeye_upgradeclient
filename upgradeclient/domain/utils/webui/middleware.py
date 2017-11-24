2d894f6fc53c8ab062775fa20bf580a8d569790d
李满满 <xmdevops@vip.qq.com>
2017-11-24 23:02:07 +0800

    :underage: perfect webui service

--- upgradeclient/domain/utils/webui/middleware.py
+++ upgradeclient/domain/utils/webui/middleware.py
@@ -15,12 +15,14 @@ render = web.template.render('{0}/'.format(template_dir), cache=False)
 class ExpMiddleware(WsgiLog):
     def __init__(self, application):
         WsgiLog.__init__(self, application, **env)
+        self.catch = self.__catch
 
-    def catch(self, environ, start_response):
+    def __catch(self, environ, start_response):
         if self.log:
             self.logger.exception(self.message)
         if self.debug:
             return render.error(content=sys.exc_info())
+
 
 
 
