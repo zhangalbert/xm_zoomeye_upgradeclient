abbe4442ad8a534d1814789a88311333ff24b222
李满满 <xmdevops@vip.qq.com>
2017-11-24 21:49:44 +0800

    :underage: perfect webui service

--- upgradeclient/domain/utils/webui/server.py
+++ upgradeclient/domain/utils/webui/server.py
@@ -5,17 +5,11 @@ import web
 
 
 from upgradeclient.domain.utils.webui.views import *
-from upgradeclient.domain.utils.webui.config import template_dir
 from upgradeclient.domain.utils.webui.middleware import LogMiddleware
 from upgradeclient.domain.utils.webui.urls import url_patterns as urls
 
 
 app = web.application(urls, globals())
-render = web.template.render('{0}/'.format(template_dir), base=os.path.join(template_dir, 'ebase'), cache=False)
-
-# 自定义404/500页面
-app.notfound = lambda: web.notfound(render.error())
-app.internalerror = lambda: web.internalerror(render.error())
 
 
 def run(*middleware, **envs):
