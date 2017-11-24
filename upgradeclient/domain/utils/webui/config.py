226c642c35903e2b4a1a42b7a0c0f7627cd36128
李满满 <xmdevops@vip.qq.com>
2017-11-24 20:46:51 +0800

    :underage: perfect webui service

--- upgradeclient/domain/utils/webui/config.py
+++ upgradeclient/domain/utils/webui/config.py
@@ -10,19 +10,20 @@ from upgradeclient.database.database import Database
 template_dir = os.path.join(Database.get_client_dir(), 'domain', 'utils', 'webui', 'templates')
 
 env = {
+    # 至少一处理器
     'log': True,
-    'logmessage': 'Server got itself in trouble',
+    'logmessage': 'web.py server got itself in trouble',
     'logname': 'web.py',
     'loglevel': logging.DEBUG,
     'logformat': '%(name)s:  %(asctime)s %(levelname)-4s %(message)s',
     'datefmt': '%a, %d %b %Y %H: %M: %S',
     'tostream': True,
-    'toprint': True,
     'prnlevel': logging.DEBUG,
 
     # 是否开启调试
     'debug': True,
+
     # 输出HTML错误
-    'tohtml': False,
+    'tohtml': True,
     'htmlfile': os.path.join(template_dir, 'error.html')
 }
