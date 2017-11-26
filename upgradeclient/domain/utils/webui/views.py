b012a8a585b9e1fdfb53f99a186c06ff26d18884
李满满 <xmdevops@vip.qq.com>
2017-11-26 18:43:22 +0800

    :underage: perfect webui service

--- upgradeclient/domain/utils/webui/views.py
+++ upgradeclient/domain/utils/webui/views.py
@@ -81,7 +81,7 @@ class ExceptionRealtimeView(BaseView):
 
         where_con = ' or '.join(map(lambda s: 'log_level=\'{0}\''.format(s), log_level))
 
-        select_storage = db.select(where=where_con, limit=log_limit, order='created_time desc')
+        select_storage = db.select(where=where_con, limit=log_limit, order='created_time')
         for ins in select_storage:
             response_data.append({
                 'id': ins.id,
