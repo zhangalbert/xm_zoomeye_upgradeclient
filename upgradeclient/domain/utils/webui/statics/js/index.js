f6aa9ca8e7718e767522c2fbdf047d6abc017e3e
李满满 <xmdevops@vip.qq.com>
2017-11-26 17:56:31 +0800

    :underage: perfect webui service

--- upgradeclient/domain/utils/webui/statics/js/index.js
+++ upgradeclient/domain/utils/webui/statics/js/index.js
@@ -15,7 +15,35 @@ $(function(){
 	});
 	// 右侧实时异常列表
     setInterval(function realtime_ajax_exception_realtime(){
-        alert('测试');
+        $.ajax({
+            type: 'GET',
+            url: '/ajax/exception/realtime',
+            data: {},
+            dataType: 'json',
+            success: function(data) {
+                var max_length = 20;
+                for (var i = 0; i < data.length; i++) {
+                    var cur_length = $('#exception_realtime section').length
+                        ,cur_ins = data[i];
+                    var html = '<section>'
+                             + '<span class="point-time text-'+cur_ins['log_level']+'">'
+                             + '<i class="fa fa-circle-o" aria-hidden="true"></i>'
+                        '    + '</span>'
+                             + '<time>'
+                        '    + '<span>'+cur_ins['created_date']+'</span>'
+                             + '<span>'+cur_ins[created_time]+'</span>'
+                             + '</time>'
+                             + '<aside>'
+                    '        + '<p class="things text-'+cur_ins['log_level']+'">'+cur_ins['log_message']+'</p>'
+                             + '</aside>'
+                             + '</section>';
+                    if (cur_length > max_length) {
+                        $('#exception_realtime section').eq(-1).remove();
+                    };
+                    $(html).insertBefore($('#exception_realtime section').eq(0))
+                }
+            }
+        })
     }, 5000);
 
 	// 固件异常趋势图
