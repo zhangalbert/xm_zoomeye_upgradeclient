965c0dec4e4bda2996725fcdc05f8eb8c0db0ba9
李满满 <xmdevops@vip.qq.com>
2017-11-26 19:04:07 +0800

    :underage: perfect webui service

--- upgradeclient/domain/utils/webui/statics/js/index.js
+++ upgradeclient/domain/utils/webui/statics/js/index.js
@@ -21,19 +21,21 @@ $(function(){
             data: {},
             dataType: 'json',
             success: function(data) {
+                var max_length = 20;
                 for (var i = 0; i < data.length; i++) {
                     var cur_ins = data[i],
                         cur_length = $('#exception_realtime section').length;
-                    var html = $('<section id="'+cur_ins['id']+'">' +
+                    var html = $('<section pk="'+cur_ins['id']+'">' +
                                  '<span class="point-time text-'+cur_ins['log_level']+'">' +
                                  '<i class="fa fa-circle-o" aria-hidden="true"></i></span>' +
                                  '<time><span>'+cur_ins['created_date']+'</span><span>'+cur_ins['created_time'] +
                                  '</span></time><aside><p class="things text-'+cur_ins['log_level']+' rtrim">' +
                                cur_ins['log_message']+'</p></aside></section>');
-                    if (cur_length == 0){
-                        $('#exception_realtime').append(html)
+                    if (cur_length > max_length) {
+                        $('#exception_realtime section').eq(-1).remove();
+                    } else if (cur_length == 0){
+                        $('#exception_realtime').append(html);
                     }else {
-                        $('#'+cur_ins['id']).remove()
                         $('#exception_realtime section').eq(0).prepend(html);
                     }
                 }
