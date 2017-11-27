9146cf42606c0d6ad1aaf38a70896a5bec3e51b7
李满满 <xmdevops@vip.qq.com>
2017-11-27 16:46:19 +0800

    :underage: fix sqlite with thread safe

--- upgradeclient/service/base_service.py
+++ upgradeclient/service/base_service.py
@@ -15,6 +15,8 @@ class BaseService(object):
         # 删除今日重复插入的内容
         today = datetime.datetime.now().strftime('%Y-%m-%d')
         when_con = 'strftime(\'%Y-%m-%d\', created_time)=\'{0}\''.format(today)
-        partial(db.delete, where=when_con, vars=parted_dict)
-
-        return partial(db.insert, **parted_dict)
+        istorage = partial(db.select, where=when_con, vars=parted_dict)
+        if istorage.first() is None:
+            return partial(db.insert, **parted_dict)
+        else:
+            return partial(db.update, where='{0},id=\'{1}\''.format(when_con, istorage.first().id), **parted_dict)
