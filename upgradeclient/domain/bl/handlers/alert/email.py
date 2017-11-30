fe6cdd8a1e351f56a46bf4e29f8c32f04ad9b9ba
李满满 <xmdevops@vip.qq.com>
2017-11-30 11:13:31 +0800

    :underage: optimization alert service

--- upgradeclient/domain/bl/handlers/alert/email.py
+++ upgradeclient/domain/bl/handlers/alert/email.py
@@ -77,8 +77,8 @@ class EmailHandler(BaseHandler):
         for d in data:
             html += '<li>'
             html += 'version: {0} # '.format(d[-4])
-            html += 'error: {0}'.format(d[-2])
-            html += '<a href="http://10.2.5.51/firmware/{0}">click</a>'.format(d[0])
+            html += 'error: {0} '.format(d[-2])
+            html += '<a href="http://10.2.5.51:8081/firmware/{0}">click</a>'.format(d[0])
             html += '</li>'
         html += '</ul>'
 
