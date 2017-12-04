6e1460945ab3c3b457233115a5e6968877107d96
李满满 <xmdevops@vip.qq.com>
2017-12-04 10:28:52 +0800

    :underage: damon for supervisord

--- upgradeclient/service/upload_service.py
+++ upgradeclient/service/upload_service.py
@@ -64,8 +64,9 @@ class UploadService(BaseService):
 
                     relation_name = ins.get_relation_name()
                     relation_type = ins.get_relation_type()
-                    if future.exception() is not None:
-                        exception = future.exception()
+                    exception = future.exception()
+                    if exception is not None:
+
                         fmtdata = (self.__class__.__name__, relation_name, relation_type, exception)
                         msgdata = '{0} upload with {1}/{2} exception, exp={3}'.format(*fmtdata)
                         self.insert_to_db(log_level='error', log_message=msgdata)
